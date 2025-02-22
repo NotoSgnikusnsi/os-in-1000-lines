baa46507171d1753a58d3458d48ca93cca8d984a
notosgnikusnsi <notosgnikusnsi@gmail.com>
2025-02-22 21:43:29 +0900

    feat: clangのファイルパスをubuntu用に変更する

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ set -xue
 QEMU=qemu-system-riscv32
 
 # clangのファイルパス (Ubuntuの場合は CC=clang)
-CC=/opt/homebrew/opt/llvm/bin/clang
+# CC=/opt/homebrew/opt/llvm/bin/clang
+CC=clang
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
